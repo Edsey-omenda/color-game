7abc81d18bed06d1de015d269ed6746ec0e91283
Edsey omenda <omendajames0317@gmail.com>
2022-06-28 23:24:27 +0300

    completed assignment

--- assets/index.js
+++ assets/index.js
@@ -1,82 +1,119 @@
-var diffEls = document.querySelectorAll(".diff__btn");
-var diffEl = document.querySelector(".diff__btn.active").innerHTML;
-var n = diffEl;
-var colorsEl = document.querySelector(".colors");
-var colorsBlocks;
-var rgbEl = document.querySelector(".rgb");
-var statusEl = document.querySelector(".status");
-var colors = [];
-var blocks = 9;
-createBlocks(n);
-resetGame();
 
-function checkColors(e) {
-  // your code here
-  
-}
 
-function resetGame() {
-  createBlocks(n);
-  document.body.style.color = "black";
-  colors = [];
-  pickColors();
-  pickedColor = random(n);
-  rgbEl.innerHTML = colors[pickedColor];
-  setColors();
-  statusEl.innerHTML =
-    "Try to guess the right color based on the RGB value by clicking on the blocks.";
-}
 
-function setColors() {
-  for (var i = 0; i < colorsBlocks.length; i++) {
-    colorsBlocks[i].style.backgroundColor = colors[i];
-  }
-}
+//colourgame design motivated by codepen.
+let blocks = 9;
+let colors = generateRandomColors(blocks);
+let block = document.querySelectorAll(".block");
+let pickedColor = pickColor();
+let rgbCode = document.getElementById("rgbCode");
+let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+let level6 = document.querySelector("#low");
+let level9 = document.querySelector("#high");
 
-function pickColors() {
-  for (var i = 0; i < n; i++) {
-    colors.push(randomColor());
-  }
-}
 
-function randomColor() {
-  return "rgb(" + random(255) + ", " + random(255) + ", " + random(255) + ")";
-}
-
-function random(r) {
-  return Math.floor(Math.random() * r);
-}
-
-function setNumberOfTiles(e) {
-  // your code here
-  var s6btn = document.querySelector("#diff__btn");
-var n9btn = document.querySelector("#diff__btn active");
-}
-
-function createBlocks(num) {
-  colorsEl.innerHTML = "";
-
-  // here is an example of a loop that is used to create the blocks of color depending on you choice ie 6 or 9, however you need to add event listeners
-  for (var i = 0; i < num; i++) {
-    var block = document.createElement("div");
-    block.classList.add("colors__block");
-    colorsEl.appendChild(block);
-  }
-  colorsBlocks = document.querySelectorAll(".colors__block");
-  for (var i = 0; i < colorsBlocks.length; i++) {
-    colorsBlocks[i].addEventListener("click", checkColors);
-  }
-}
-
-n9btn.addEventListener("click", function(){
-  s6btn.classList.remove("selected");
-  n9btn.classList.add("selected");
-  blocks = 9;
-  colors = randomColor(blocks);
-  pickedColor = pickColors();
+level6.addEventListener("click", function(){
+  level9.classList.remove("selected");
+  level6.classList.add("selected");
+  blocks = 6;
+  colors = generateRandomColors(blocks);
+  pickedColor = pickColor();
   rgbCode.textContent = pickedColor;
-  for(var i = 0; i < blocks.length; i++) {
-      blocks[i].style.background = colors[i];
-      blocks[i].style.display = "block";
+  for(let i = 0; i < block.length; i++) {
+    if (colors[i]) {
+      block[i].style.background = colors[i];
+    } else {
+      block[i].style.display = "none";
+    }
   }
 });
+
+level9.addEventListener("click", function(){
+  level6.classList.remove("selected");
+  level9.classList.add("selected");
+  blocks = 9;
+  colors = generateRandomColors(blocks);
+  pickedColor = pickColor();
+  rgbCode.textContent = pickedColor;
+  for(var i = 0; i < block.length; i++) {
+      block[i].style.background = colors[i];
+      block[i].style.display = "block";
+  }
+});
+
+resetButton.addEventListener("click", function(){
+  
+  colors = generateRandomColors(blocks);
+ 
+  pickedColor = pickColor();
+ 
+  rgbCode.textContent = pickedColor;
+  this.textContent = "New Color";
+  messageDisplay.textContent = "";
+ 
+  for (let i = 0; i < block.length; i++) {
+    block[i].style.background = colors[i];
+  }
+  h1.style.background = "brown";
+})
+
+rgbCode.textContent = pickedColor;
+
+for(let i = 0; i < block.length; i++) {
+ 
+  block[i].style.background = colors[i];
+  
+  block[i].addEventListener("click", function() {
+   
+    let clickedColor = this.style.background;
+   
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = "Congratulations";
+      resetButton.textContent = "Play Again ?";
+      changeColors(clickedColor);
+      h1.style.background = clickedColor;
+    }else{
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Don't quit";
+    }
+  })
+}
+
+function changeColors(color){
+  
+for (let i = 0; i < block.length; i++) {
+ 
+    block[i].style.background = color;
+}
+
+}
+
+function pickColor(){
+var random = Math.floor(Math.random() * colors.length)
+return colors[random];
+}
+
+function generateRandomColors(num){
+ 
+  let colorArray = []
+
+  for (let i = 0; i < num; i++) {
+    colorArray.push(randomColor())
+   
+  }
+ 
+  return colorArray;
+}
+
+function randomColor(){
+  
+let r = Math.floor(Math.random() * 256)
+ 
+let g = Math.floor(Math.random() * 256)
+  
+let b = Math.floor(Math.random() * 256)
+
+return "rgb(" + r +", " + g + ", " + b +")";
+}
